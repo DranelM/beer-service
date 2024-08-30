1b22d4b03e99381bac53264f41e7b28eca3898d1
Lenny <maciej.lenard1@gmail.com>
2024-08-30 12:54:05 +0200

    Exception Handler

--- src/main/java/dranelm/beer_service/web/controller/BeerController.java
+++ src/main/java/dranelm/beer_service/web/controller/BeerController.java
@@ -3,6 +3,7 @@ package dranelm.beer_service.web.controller;
 import dranelm.beer_service.web.model.BeerDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,13 +25,13 @@ public class BeerController {
     }
 
     @PostMapping
-    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto) {
+    public ResponseEntity saveNewBeer(@Validated  @RequestBody BeerDto beerDto) {
 //        todo add impl
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
-    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
+    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @Validated @RequestBody BeerDto beerDto) {
 //        todo add impl
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
